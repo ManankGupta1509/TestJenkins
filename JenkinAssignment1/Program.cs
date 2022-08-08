6c4b8869d2ba0d81373ccc47b091638c114fae3e
ManankGupta1509 <manankgupta009@gmail.com>
2022-08-08 16:36:45 +0530

    new

---
+++ JenkinAssignment1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JenkinAssignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test project for Jenkin Assignment");
+        }
+    }
+}
